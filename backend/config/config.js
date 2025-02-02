1417c8c241b131406fabc64ab740c67e7334d01f
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-02 02:27:35 -0800

    update

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 5000,  // El puerto donde corre el servidor
-  mongoURI: 'mongodb+srv://emanuel:javicarriendo@cluster0.t0dck.mongodb.net/',  // URL para la conexión a MongoDB Atlas
+  mongoURI: 
 };
 
 module.exports = config;

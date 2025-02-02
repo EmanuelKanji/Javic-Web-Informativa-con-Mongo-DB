95d37e9b4d0b09a0bc5230d74aa2af35e6c32c29
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-01 23:24:23 -0800

    modificado el server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ console.log("🔍 MONGO_URI:", process.env.MONGO_URI);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('./config/config');  // 📌 Importar config.js
-require('./config/db');  // 📌 Conectar a la base de datos
+const config = require('./config/config');  // 📌 Cambié la ruta para apuntar a config.js en la raíz
+require('./config/db');  // 📌 Conectar a la base de datos, también se asume que esta carpeta está en la raíz
 
 const app = express();
 const PORT = config.port;
@@ -13,10 +13,10 @@ const PORT = config.port;
 app.use(cors());
 app.use(express.json());
 
-const contactRoutes = require('./routes/contactRoutes');
+const contactRoutes = require('./routes/contactRoutes');  // 📌 También cambié la ruta de contactRoutes
 app.use('/api/contacto', contactRoutes);  // 📌 La API tendrá el prefijo "/api"
-
 
 app.listen(PORT, () => {
     console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
+

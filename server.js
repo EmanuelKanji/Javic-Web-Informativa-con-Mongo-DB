19bd979de1008f7fe0e25b682c01323bf0c17a39
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-02 12:35:08 -0800

    update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('./config');
-require('./db');  // Conectar a la base de datos de MongoDB
+const config = require('./backend/config/config');
+require('./backend/config/db');  // Conectar a la base de datos de MongoDB
 
 const app = express();
 const PORT = config.port;

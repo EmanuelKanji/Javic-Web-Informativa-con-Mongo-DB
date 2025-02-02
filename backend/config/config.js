3eb96f88bdf2ca9c33562630948a5d121a88d1b0
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-02 02:29:21 -0800

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 5000,  // El puerto donde corre el servidor
-  mongoURI: 
+  mongoURI: process.env.MONGO_URI  // Usamos la variable de entorno que contiene la URL de conexión
 };
 
 module.exports = config;

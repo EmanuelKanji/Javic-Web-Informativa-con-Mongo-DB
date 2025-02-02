fa297469555baf013860e8bd06e6785ec4fdeb84
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-02-01 23:38:50 -0800

    update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ console.log("🔍 MONGO_URI:", process.env.MONGO_URI);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('./config/config'); 
-require('./config/db');  
+const config = require('./backend/config');
+require('./backend/db'); 
 
 const app = express();
 const PORT = config.port;
@@ -13,10 +13,11 @@ const PORT = config.port;
 app.use(cors());
 app.use(express.json());
 
-const contactRoutes = require('./routes/contactRoutes');  
-app.use('/api/contacto', contactRoutes);  
+const contactRoutes = require('./backend/routes/contactRoutes');
+app.use('/api/contacto', contactRoutes);
 
 app.listen(PORT, () => {
     console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
+
 

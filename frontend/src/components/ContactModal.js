7402ce3670fce54ad2fea61c4d41e1ab9b94b846
Emanuel Aguilera <emanuel.aguilera.escarate@gmail.com>
2025-08-08 17:00:16 -0400

    Solucion de comunicacion del formulario al backend

--- frontend/src/components/ContactModal.js
+++ frontend/src/components/ContactModal.js
@@ -102,7 +102,7 @@ function ContactModal({ isOpen, onClose }) {
     }
 
     try {
-      const response = await fetch("http://localhost:10000/api/contacto", {
+      const response = await fetch("https://javic-backend.onrender.com/api/contacto", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
